6e32a49adfd30d7bc9d5f7f6c113896842723d96
Alex <freesafesoft@gmail.com>
2018-11-29 11:31:02 +0100

    новая активность с рисунком (02)

--- app/src/main/java/com/example/android/adcent_calendar_2018/MainActivity.java
+++ app/src/main/java/com/example/android/adcent_calendar_2018/MainActivity.java
@@ -2,6 +2,18 @@ package com.example.android.adcent_calendar_2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNewDay (View v){
+
+        startActivity(new Intent(getApplicationContext(), newDay.class));
+
     }
 }
---
+++ app/src/main/java/com/example/android/adcent_calendar_2018/newDay.java
@@ -0,0 +1,13 @@
+package com.example.android.adcent_calendar_2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newDay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_day);
+    }
+}
